f9c0e2bf9f0364ffaa6cb2ad281d72d949c8855f
Anderson Leite <andersonleite.dev@gmail.com>
2024-07-21 08:39:09 -0300

    feat: add action container and update list component layout

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -15,7 +17,7 @@ export class ListComponent {
   productService = inject(ProductsService);
 
   ngOnInit() {
-    this.productService.getAllProducts().subscribe((products) => {
+    this.productService.getAllProducts().subscribe((products: Product[]) => {
       this.products = products;
     });
   }
