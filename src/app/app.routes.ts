ffd3d675e95b60a2199f95491915291d98de5248
Anderson Leite <andersonleite.dev@gmail.com>
2024-07-22 06:54:13 -0300

    feat: add EditComponent and routing for editing products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
 import { CreateComponent } from './features/create/create.component';
+import { EditComponent } from './features/edit/edit.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'add-product',
     component: CreateComponent
+  },
+  {
+    path: 'edit-product',
+    component: EditComponent
   }
 ];
