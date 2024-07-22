6c1f94719cdfb9720051dc8dad25b70348dccea9
Anderson Leite <andersonleite.dev@gmail.com>
2024-07-22 05:19:03 -0300

    feat: add name field to create component form

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-create',
@@ -20,14 +21,22 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class CreateComponent {
 
+  productService = inject(ProductsService);
+
   form = new FormGroup({
-    title: new FormControl<string>('', {
+    name: new FormControl<string>('', {
       nonNullable: true,
       validators: Validators.required,
     })
   });
 
   onSubmit() {
-    this.form.controls.title.value;
+    this.productService.post({ 
+      name: this.form.controls.name.value 
+    })
+    .subscribe(() => {
+      alert('Produto salvo com sucesso!');
+      this.form.reset()
+    });
   }
 }
--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
+import { ProductPayload } from '../interfaces/payload.product.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,10 @@ export class ProductsService {
 
   getAllProducts() {
     return this.httpClient.get<Product[]>('/api/products');
+  }
+
+  post(product: ProductPayload) {
+    return this.httpClient.post('/api/products', product);
   }
   
 }
