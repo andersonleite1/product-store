c9d7b1b66ba894095bdfe12811110347451c62c7
Anderson Leite <andersonleite.dev@gmail.com>
2024-07-22 05:33:25 -0300

    feat: add form validation and error handling to create component

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { ProductsService } from '../../shared/services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -21,7 +23,11 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class CreateComponent {
 
+  router = inject(Router);
+
   productService = inject(ProductsService);
+
+  matSnackBar = inject(MatSnackBar);
 
   form = new FormGroup({
     name: new FormControl<string>('', {
@@ -31,12 +37,23 @@ export class CreateComponent {
   });
 
   onSubmit() {
+
+    if (this.form.invalid) {
+      this.matSnackBar.open('Preencha o campo nome', 'OK', {
+        duration: 3000,
+      });
+      return;
+    }
+
     this.productService.post({ 
       name: this.form.controls.name.value 
     })
     .subscribe(() => {
-      alert('Produto salvo com sucesso!');
+      this.matSnackBar.open('Produto cadastrado com sucesso', 'OK', {
+        duration: 3000,
+      });
       this.form.reset()
+      this.router.navigate(['/']);
     });
   }
 }
